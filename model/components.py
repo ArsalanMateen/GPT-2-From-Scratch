fa12c16b88949b330926f4610866a5168efc4612
Arsalan Mateen <arsalanmateen.pk@gmail.com>
2026-01-03 18:49:12 +0500

    evaluate model performance

--- model/components.py
+++ model/components.py
@@ -195,7 +195,7 @@ class GPTModel(nn.Module):
         return logits
 
 """
-Generating Text from model
+Generating text from model
 (logits to text, greedy decoding)
 """
 def generate_text(model, idx, max_new_tokens, context_size): 
@@ -226,4 +226,34 @@ def text_to_token_ids(raw_text, tokenizer):
 def token_ids_to_text(token_ids, tokenizer):
     flat = token_ids.squeeze(0)               
     return tokenizer.decode(flat.tolist())
-    
+
+"""
+Model Evaluation
+"""
+def calc_batch_loss(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)        
+    target_batch = target_batch.to(device)      
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1), target_batch.flatten()
+    )
+    return loss
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)    
+    else:
+        num_batches = min(num_batches, len(data_loader))  
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_batch_loss(
+                input_batch, target_batch, model, device
+            )
+            total_loss += loss.item()   
+        else:
+            break
+    return total_loss / num_batches
+
